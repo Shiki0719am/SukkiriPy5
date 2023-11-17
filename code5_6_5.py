4d7d4db1924782c4c85b4a04962f9313232f5f38
Shiki0719am <kunuto0719am@gmail.com>
2023-11-17 11:50:28 +0900

    5章終わり

--- code5_6_5.py
+++ code5_6_5.py
@@ -1,24 +1,23 @@
 def int_input(text):
     return int(input(text))
 
-def calc_payment(many, menber):
+
+
+
+def calc_payment(many, menber = 2):
     dnum = many/menber
-    pay = dnum // 100 * 100
-    if dnum > pay:
-        pay = int(pay + 100)
-    x = many - pay * (menber - 1)
+    ans = dnum // 100 * 100
+    if dnum > ans:
+        ans = int(ans + 100)
+    monyorg = many - ans * (menber - 1)
+    return int(ans), int(monyorg)
+
+def show_patment(pay,payorg,people = 2):
+    print(f"1人あたり{pay}円（{people-1}）人、幹事は{payorg}です")
+
 
 
 amount = int(input("支払総額を入れてください"))
 people = int(input("参加人数を入力してください"))
-
-
-dnum = amount/people
-pay = dnum // 100 * 100
-if dnum > pay:
-    pay = int(pay + 100)
-
-payorg = amount - pay * (people - 1)
-
-print("支払額")
-print(f"1人あたり{pay}円{people-1}、幹事は{payorg}円です")
+pay,payorg = calc_payment(amount,people)
+show_patment(pay,payorg,people)
